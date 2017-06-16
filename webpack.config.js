5b0885e1f160523c4498e1fee9be1e891691d444
Magnus Nilsson <magnus@tretton37.com>
2017-06-16 12:03:42 +0200

    Switched to webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var WWW_ROOT = path.resolve(__dirname);
+var APP_DIR = path.resolve(__dirname, 'src');
+var MODULES_DIR = path.resolve(__dirname, 'node_modules');
+
+module.exports = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    filename: 'app.js',
+    path: WWW_ROOT + '/build'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader'
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css'),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: APP_DIR + '/index.html',
+      filename: WWW_ROOT + '/build/index.html',
+      hash: true
+  })],
+  resolve: {
+    extensions: [".js", ".json", ".jsx"]
+  }
+};
